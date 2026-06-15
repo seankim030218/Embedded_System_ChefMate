5a064128be07697178374fc25d95c827515d364c
seankim030218 <seankim0218@gmail.com>
2026-06-15 15:39:26 +0900

    fix: 기울기 센서 채터링 방지 (상태 변화 시에만 UART 전송)

--- UART_Tilt.ino
+++ UART_Tilt.ino
@@ -5,6 +5,7 @@
 #define SS_TX 8
 
 SoftwareSerial mySerial(SS_RX, SS_TX);
+int lastSentState = -1;
 
 void setup() {
   pinMode(TILT_PIN, INPUT_PULLUP);
@@ -13,10 +14,13 @@ void setup() {
 
 void loop() {
   int tiltState = digitalRead(TILT_PIN);
-  if (tiltState == LOW) {
-    mySerial.print("T1#");
-  } else {
-    mySerial.print("T0#");
+  if (tiltState != lastSentState) {
+    if (tiltState == LOW) {
+      mySerial.print("T1#");
+    } else {
+      mySerial.print("T0#");
+    }
+    lastSentState = tiltState;
   }
-  delay(200);
+  delay(50);
 }
